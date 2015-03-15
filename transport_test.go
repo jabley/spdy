a38211d9015bd2a065993d55357187d1c131c82c
James Abley <james.abley@gmail.com>
2015-03-15 12:47:48 +0000

    Restructure tests
    Got ~68% coverage at the moment. That’s because I’ve implemented bits
    of the spec without testing them. Error handling is always the fun part.

--- transport_test.go
+++ transport_test.go
@@ -7,16 +7,22 @@ import (
 )
 
 func TestTransportExternal(t *testing.T) {
-	// req, _ := http.NewRequest("GET", "https://www.performance.service.gov.uk/data/transactional-services/summaries?sort_by=_timestamp:descending&filter_by=service_id:dh-blood-donation-appointments&filter_by=type:seasonally-adjusted", nil)
-	// req, _ := http.NewRequest("GET", "https://127.0.0.1:10443/", nil)
-	// req, _ := http.NewRequest("GET", "http://queue.acm.org/", nil)
-	req, _ := http.NewRequest("GET", "https://www.gov.uk/", nil)
-	rt := &Transport{
-		InsecureTLSDial: true,
+	URLs := []string{
+		"https://www.performance.service.gov.uk/data/transactional-services/summaries?sort_by=_timestamp:descending&filter_by=service_id:dh-blood-donation-appointments&filter_by=type:seasonally-adjusted",
+		// "https://127.0.0.1:10443/",
+		"http://queue.acm.org/",
+		"https://www.gov.uk/",
 	}
-	res, err := rt.RoundTrip(req)
-	if err != nil {
-		t.Fatalf("%v", err)
+
+	for _, URL := range URLs {
+		req, _ := http.NewRequest("GET", URL, nil)
+		rt := &Transport{
+			InsecureTLSDial: true,
+		}
+		res, err := rt.RoundTrip(req)
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		res.Write(os.Stdout)
 	}
-	res.Write(os.Stdout)
 }
